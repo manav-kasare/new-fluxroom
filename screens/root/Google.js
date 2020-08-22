a124b8ac6922c3f1c782894637f623d5c3759267
manav-kasare <kmanav0705@gmail.com>
2020-08-22 19:30:01 +0530

    Added(halfway) Google SignIn

--- screens/root/Google.js
+++ screens/root/Google.js
@@ -1,8 +1,94 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import {TouchableOpacity, Linking} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import {Auth, Hub} from 'aws-amplify';
+import {withOAuth} from 'aws-amplify-react-native';
+import {UserDetailsContext} from '../../shared/Context';
+import CustomToast from '../../shared/CustomToast';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
+import Amplify from 'aws-amplify';
+import awsconfig from '../../aws-exports';
 
-export default function Google() {
+import {getUserInfo, createUser} from '../../backend/database/apiCalls';
+
+async function urlOpener(url, redirectUrl) {
+  await InAppBrowser.isAvailable();
+  const {type, url: newUrl} = await InAppBrowser.openAuth(url, redirectUrl, {
+    showTitle: false,
+    enableUrlBarHiding: true,
+    enableDefaultShare: false,
+    ephemeralWebSession: false,
+  });
+
+  if (type === 'success') {
+    Linking.openURL(newUrl);
+  }
+}
+
+Amplify.configure({
+  ...awsconfig,
+  oauth: {
+    ...awsconfig.oauth,
+    urlOpener,
+  },
+});
+
+const Google = () => {
+  const {setUser} = React.useContext(UserDetailsContext);
+
+  const handleCognitoHostedUI = () => {
+    getUser().then((userData) => {
+      console.log(userData);
+      // const identities = JSON.parse(userData.identities)[0];
+      // const attributes = userData.attributes;
+      // getUserInfo(identities.userId).then((responseData) => {
+      //   if (responseData.id) {
+      //     setUser(responseData);
+      //   } else {
+      //     createUser({
+      //       id: identities.userId,
+      //       email: attributes.email,
+      //     }).then(({data}) => {
+      //       if (data.error) {
+      //         CustomToast('An Error Occured');
+      //       } else {
+      //         navigation.navigate('SetUpProfile', {
+      //           id: identities.userId,
+      //         });
+      //       }
+      //     });
+      //   }
+      // });
+    });
+  };
+
+  React.useEffect(() => {
+    Hub.listen('auth', ({payload: {event, data}}) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          handleCognitoHostedUI();
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'signIn_failure':
+          CustomToast('An Error Occured');
+        case 'cognitoHostedUI_failure':
+          CustomToast('Sign in failure', data);
+          break;
+      }
+    });
+
+    handleCognitoHostedUI();
+  }, []);
+
+  const getUser = () => {
+    return Auth.currentAuthenticatedUser()
+      .then((userData) => userData)
+      .catch(() => console.log('Not signed in'));
+  };
+
   return (
     <TouchableOpacity
       style={{
@@ -12,8 +98,11 @@ export default function Google() {
         backgroundColor: '#4640C1',
         alignItems: 'center',
         justifyContent: 'center',
-      }}>
+      }}
+      onPress={() => Auth.federatedSignIn({provider: 'Google'})}>
       <AntDesign name="google" size={25} color="white" />
     </TouchableOpacity>
   );
-}
+};
+
+export default withOAuth(Google);
