560114429213f71e2f431f1d04e76e317ad509e5
manav-kasare <kmanav0705@gmail.com>
2020-09-22 20:21:43 +0530

    Fixed Android Errors

--- android/app/src/main/java/com/fluxroom/MainApplication.java
+++ android/app/src/main/java/com/fluxroom/MainApplication.java
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          new RNGoogleSigninPackage() 
+          new RNGoogleSigninPackage();
           return packages;
         }
 
@@ -36,8 +36,6 @@ public class MainApplication extends Application implements ReactApplication {
           return "index";
         }
       };
-
-  ....
 
   @Override
   public ReactNativeHost getReactNativeHost() {
